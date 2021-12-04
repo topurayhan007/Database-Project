229bd056ada8aa749546672ecf2b84c78cb45423
topurayhan007 <topurayhan007@gmail.com>
2021-12-05 01:45:19 +0600

    course.py

--- Student Enrollment Analysis System/scripts/course.py
+++ Student Enrollment Analysis System/scripts/course.py
@@ -1,20 +1,20 @@
-
 #importing pandas as pd
 import pandas as pd
+from pandas import read_csv, read_excel, to_csv
  
 # Read and store content
 # of an excel file
-read_file = pd.read_excel ("D:/STUDY\CSE303 (Database)/Database-Project/Student Enrollment Analysis System/scripts/classSize.xlsx", engine= 'openpyxl')
+read_file = pd.read_excel("scripts/classSize.xlsx", engine= 'openpyxl')
  
 # Write the dataframe object
 # into csv file
-read_file.to_csv ("D:/STUDY\CSE303 (Database)/Database-Project/Student Enrollment Analysis System/scripts/classSize.csv",
+read_file.to_csv ("scripts/classSize.csv",
                   index = None,
                   header=True)
    
 # read csv file and convert
 # into a dataframe object
-df = pd.DataFrame(pd.read_csv("D:/STUDY\CSE303 (Database)/Database-Project/Student Enrollment Analysis System/scripts/classSize.csv"))
+df = pd.DataFrame(pd.read_csv("scripts/classSize.csv"))
  
 # show the dataframe
 print(df)
