4e49cd46bd643437b53d45a736b65f9e4ea47ead
topurayhan007 <topurayhan007@gmail.com>
2021-12-06 14:43:53 +0600

    Changed back to SQLite3

--- Student Enrollment Analysis System/Home/settings.py
+++ Student Enrollment Analysis System/Home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -75,18 +75,23 @@ WSGI_APPLICATION = 'Home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': config('DB_NAME'),  
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),  
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }  
-} 
+#DATABASES = {  
+#    'default': {  
+#        'ENGINE': 'django.db.backends.mysql',  
+#        'NAME': config('DB_NAME'),  
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),  
+#        'HOST': config('DB_HOST'),
+#        'PORT': config('DB_PORT'),
+#    }  
+#} 
 
-
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
