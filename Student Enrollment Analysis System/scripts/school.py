0bfc646c0ca725030f88d73c4aa0bdeaf1d9d6fd
topurayhan007 <topurayhan007@gmail.com>
2021-12-09 18:06:11 +0600

    edited deptarment

--- Student Enrollment Analysis System/scripts/school.py
+++ Student Enrollment Analysis System/scripts/school.py
@@ -1,17 +1,21 @@
 import pandas as pd
-from seas.models import School_T
+from seas.models import Department_T, School_T
 
 data = pd.read_excel('scripts/Revenue.xlsx', sheet_name='Data')
-data = set(data.SCHOOL_TITLE)
+data = data[['SCHOOL_TITLE', 'Dept']]
 
-for item in data:
-    if item == "SPPH":
-        School_T(schoolTitle=item, schoolName = "School of Pharmacy and Public Health").save()
-    elif item == "SLASS":
-        School_T(schoolTitle=item, schoolName = "School of Liberal Arts & Social Sciences").save()
-    elif item == "SETS":
-        School_T(schoolTitle=item, schoolName = "School of Engineering, Technology & Sciences").save()
-    elif item == "SELS":
-        School_T(schoolTitle=item, schoolName = "School of Environment and Life Sciences").save()
-    elif item == "SBE":
-        School_T(schoolTitle=item, schoolName = "School of Business and Entrepreneurship").save()
+#previously thought needed to parse dept_code from courseID
+# for i in range(data.shape[0]):
+#     data.iloc[i, 1] = data.iloc[i, 1][:3]
+
+data = data.drop_duplicates()
+
+for i in range(data.shape[0]):
+    if data.iloc[i, 1] == "CSE":
+        Department_T(deptCode=data.iloc[i, 1], deptName= "Department of Computer Science and Engineering", school=School_T.objects.get(schoolTitle=data.iloc[i, 0])).save()
+    elif data.iloc[i, 1] == "EEE":
+        Department_T(deptCode=data.iloc[i, 1], deptName= "Department of Electrical and Electronic Engineering", school=School_T.objects.get(schoolTitle=data.iloc[i, 0])).save()
+    elif data.iloc[i, 1] == "PhySci":
+        Department_T(deptCode=data.iloc[i, 1], deptName= "Department of Physical Sciences", school=School_T.objects.get(schoolTitle=data.iloc[i, 0])).save()
+    else:
+        Department_T(deptCode=data.iloc[i, 1], school=School_T.objects.get(schoolTitle=data.iloc[i, 0])).save()
