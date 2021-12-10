452031f359b63e71b8d0cee329ecdaeaaaf2dcea
topurayhan007 <topurayhan007@gmail.com>
2021-12-10 21:42:59 +0600

    Updated Section population

--- Student Enrollment Analysis System/scripts/section.py
+++ Student Enrollment Analysis System/scripts/section.py
@@ -13,11 +13,12 @@
 import pandas as pd
 from seas.models import Course_T, Section_T, Classroom_T, Faculty_T
 
-data = pd.read_excel('scripts/Revenue.xlsx', sheet_name='Data')
-df2 = data.FACULTY_FULL_NAME.str.split("-", expand=True)
-data['FACULTY_ID'] = df2[0]
-data['FACULTY_NAME'] = df2[1]
-data.to_excel('scripts/Revenue_Faculty.xlsx', index = None, header=True)
+df = pd.read_excel('scripts/Revenue.xlsx', sheet_name='Data')
+df2 = df.FACULTY_FULL_NAME.str.split("-", expand=True)
+df['FACULTY_ID'] = df2[0]
+df['FACULTY_NAME'] = df2[1]
+df.to_excel('scripts/Revenue_Faculty.xlsx', index = None, header=True)
+print("Excel Update Success")
 
 data = pd.read_excel('scripts/Revenue_Faculty.xlsx')
 
@@ -26,15 +27,18 @@ data = data[['CourseID', 'Sec', 'ROOM_ID', 'size', 'stuNo', 'Semester', 'Year',
 data = data.drop_duplicates()
 
 for index, row in data.iterrows():
+    print('Inserting')
     Section_T(course = Course_T.objects.get(courseID = row['CourseID']),
-             sectionNo = row('Sec'),
+             sectionNo = row['Sec'],
              room = Classroom_T.objects.get(roomID = row['ROOM_ID']),
              capacity = row['size'],
              noOfEnrolledStudent = row['stuNo'],
              semester = row['Semester'],
              year = row['Year'],
-             facultyID = Faculty_T.objects.get(facultyID = row['FACULTY_ID']),
+             faculty = Faculty_T.objects.get(facultyID = row['FACULTY_ID']),
              startTime = row['STRAT_TIME'],
              endTime = row['END_TIME'],
              day = row['ST_MW'],
              blocked = row['BLOCKED']).save()
+
+print("Success!")
