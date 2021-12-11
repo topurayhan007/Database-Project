51d6820e47e3a2dd5b9e03dedbdf71ad42860f85
topurayhan007 <topurayhan007@gmail.com>
2021-12-11 21:54:35 +0600

    updated PopulationScript from sections.py

--- Student Enrollment Analysis System/scripts/populationScript.py
+++ Student Enrollment Analysis System/scripts/populationScript.py
@@ -88,31 +88,38 @@ print("Faculty Population Successful!")
 
 
 # Section Table Population
-df = pd.read_excel('scripts/Revenue.xlsx', sheet_name='Data')
-df2 = df.FACULTY_FULL_NAME.str.split("-")
-df['FACULTY_ID'] = df2[0]
-df['FACULTY_NAME'] = df2[1]
-df.to_excel('scripts/Revenue_Faculty.xlsx', index = None, header=True)
-print("Excel Update Success")
-
 data = pd.read_excel('scripts/Revenue_Faculty.xlsx')
 
 data = data[['CourseID', 'Sec', 'ROOM_ID', 'size', 'stuNo', 'Semester', 'Year', 
             'FACULTY_ID', 'STRAT_TIME', 'END_TIME', 'ST_MW', 'BLOCKED']]
-# data = data.drop_duplicates()
+data = data.drop_duplicates()
 
 for index, row in data.iterrows():
-    Section_T(course = Course_T.objects.get(courseID = row['CourseID']),
-             sectionNo = int(row['Sec']),
-             room = Classroom_T.objects.get(roomID = row['ROOM_ID']),
-             capacity = row['size'],
-             noOfEnrolledStudent = row['stuNo'],
-             semester = row['Semester'],
-             year = row['Year'],
-             faculty = Faculty_T.objects.get(facultyID = row['FACULTY_ID']),
-            #  startTime = row['STRAT_TIME'],
-            #  endTime = row['END_TIME'],
-             day = row['ST_MW'],
-             blocked = row['BLOCKED']).save()
+    try:
+        Section_T(course = Course_T.objects.get(courseID = row['CourseID']),
+        sectionNo = row['Sec'],
+        room = Classroom_T.objects.get(roomID = row['ROOM_ID']),
+        capacity = row['size'],
+        noOfEnrolledStudent = row['stuNo'],
+        semester = row['Semester'],
+        year = row['Year'],
+        faculty = Faculty_T.objects.get(facultyID = row['FACULTY_ID']),
+        # startTime = row['STRAT_TIME'],
+        # endTime = row['END_TIME'],
+        day = row['ST_MW'],
+        blocked = row['BLOCKED']).save()
+
+    except ValueError:
+        Section_T(course = Course_T.objects.get(courseID = row['CourseID']),
+        sectionNo = row['Sec'],
+        room = Classroom_T.objects.get(roomID = row['ROOM_ID']),
+        capacity = row['size'],
+        noOfEnrolledStudent = row['stuNo'],
+        semester = row['Semester'],
+        year = row['Year'],
+        # startTime = row['STRAT_TIME'],
+        # endTime = row['END_TIME'],
+        day = row['ST_MW'],
+        blocked = row['BLOCKED']).save()
 
 print("Section Population Successful!")
