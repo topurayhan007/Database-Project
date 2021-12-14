8e5e5d355ca5d8596a03e9d199ac6f1df4c1c831
topurayhan007 <topurayhan007@gmail.com>
2021-12-14 19:49:38 +0600

    wrote func for ClassSizeRequirement & ClsDist & UsageOfTheResources

--- Student Enrollment Analysis System/seas/chartQueries.py
+++ Student Enrollment Analysis System/seas/chartQueries.py
@@ -105,15 +105,15 @@ def ClassSizeRequirement(semester, year):
 # print(resu)
 
 
-def ClassSizeDistribution(semester, year):
+def ClassSizeDistribution(semester, year, school):
     with connection.cursor() as cursor:
         cursor.execute('''
-            SELECT COUNT(*) AS School_SBE
+            SELECT COUNT(*)
             FROM (SELECT *
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 1 AND 10
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -122,7 +122,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 11 AND 20
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -131,7 +131,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 21 AND 30
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -140,7 +140,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 31 AND 35
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -149,7 +149,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 36 AND 40
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -158,7 +158,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 41 AND 50
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -167,7 +167,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 51 AND 55
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -176,7 +176,7 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent BETWEEN 56 AND 60
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
             ------------------------------------------------------------------------------------
             UNION ALL
             ------------------------------------------------------------------------------------
@@ -185,10 +185,105 @@ def ClassSizeDistribution(semester, year):
                 FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
                 INNER JOIN seas_section_t ON course_id=courseid)
             WHERE noofenrolledstudent > 60
-            AND semester = "Summer" AND YEAR = '2021' AND school_id = "SBE"
-        '''.format(semester,year,semester,year,semester,year,semester,year,semester,year,semester,year,semester,year,semester,year))
+            AND semester = "{}" AND YEAR = '{}' AND school_id = "{}"
+        '''.format(semester,year,school, semester,year,school, semester,year,school, semester,year,school, semester,year,school, semester,year,school, semester,year,school, semester,year,school, semester,year,school))
         result = cursor.fetchall()
     return result
 
 
-#print(ClassSizeRequirement("Summer", '2021'))
+# print(ClassSizeDistribution("Spring", '2021', "SETS"))
+# sbe = ClassSizeDistribution("Spring", '2021', "SBE")
+# sels = ClassSizeDistribution("Spring", '2021', "SELS")
+# sets = ClassSizeDistribution("Spring", '2021', "SETS")
+# slass = ClassSizeDistribution("Spring", '2021', "SLASS")
+# spph = ClassSizeDistribution("Spring", '2021', "SPPH")
+# allarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
+# print(allarr)
+# totalarr = allarr.sum(axis=1)
+# print(totalarr)
+# finalarr = np.concatenate((allarr,totalarr),axis=1)
+# print(finalarr)
+
+
+
+def UsageOfTheResources(semester, year):
+    with connection.cursor() as cursor:
+        cursor.execute('''
+            SELECT 
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}'
+            ------------------------------------------------------------------------------------
+            UNION ALL
+            ------------------------------------------------------------------------------------
+            SELECT
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}' AND school_id = "SBE"
+            ------------------------------------------------------------------------------------
+            UNION ALL
+            ------------------------------------------------------------------------------------
+            SELECT
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}' AND school_id = "SELS"
+            ------------------------------------------------------------------------------------
+            UNION ALL
+            ------------------------------------------------------------------------------------
+            SELECT
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}' AND school_id = "SETS"
+            ------------------------------------------------------------------------------------
+            UNION ALL
+            ------------------------------------------------------------------------------------
+            SELECT
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}' AND school_id = "SLASS"
+            ------------------------------------------------------------------------------------
+            UNION ALL
+            ------------------------------------------------------------------------------------
+            SELECT
+            ROUND(sum(capacity),2),
+            ROUND(avg(noofenrolledstudent),2),
+            ROUND(avg(capacity),2),
+            ROUND((avg(capacity)-avg(noofenrolledstudent)),2),
+            ROUND(((avg(capacity)-avg(noofenrolledstudent))/avg(capacity)*100),2)
+            FROM (SELECT *
+                FROM seas_department_t INNER JOIN seas_course_t ON deptcode=dept_id
+                INNER JOIN seas_section_t ON course_id=courseid)
+            WHERE semester = "{}" AND YEAR = '{}' AND school_id = "SPPH"
+        '''.format(semester,year,semester,year,semester,year,semester,year,semester,year,semester,year))
+        result = cursor.fetchall()
+    return result
--- Student Enrollment Analysis System/seas/views.py
+++ Student Enrollment Analysis System/seas/views.py
@@ -35,12 +35,35 @@ def logout_request(request):
 	messages.info(request, "You have successfully logged out.") 
 	return redirect('login')
 
+
 def ClassSizeRequirementView(request):
     if request.method == "POST":
-        # get.year from HTML DropDown Selection
+        # get.year and get.semester from HTML DropDown Selection put that instead of Spring and 2021
         arr1 = chartQueries.ClassSizeRequirement("Spring", '2021')
         arr2 = chartQueries.ClassSizeRequirement("Spring", '2021')
         arr3 = np.concatenate((arr1, arr2), axis=1)
-        sumarr = arr3.sum(axis=0)
-        table = np.concatenate((arr3, sumarr), axis=0)
-        return render(request, 'chartLayout.html', {"result":table})
+        totalarr = arr3.sum(axis=0)
+        #line 47 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
+        table = np.concatenate((arr3, totalarr), axis=0)
+
+    # Note here "result" is the variable by which the HTML will recognize "table"   
+    return render(request, 'chartLayout.html', {"result":table})
+
+
+def ClassSizeDistributionView(request):
+    if request.method == 'POST':
+        # get.year and get.semester from HTML DropDown Selection and put that instead of Spring and 2021
+        sbe = chartQueries.ClassSizeDistribution("Spring", '2021', "SBE")
+        sels = chartQueries.ClassSizeDistribution("Spring", '2021', "SELS")
+        sets = chartQueries.ClassSizeDistribution("Spring", '2021', "SETS")
+        slass = chartQueries.ClassSizeDistribution("Spring", '2021', "SLASS")
+        spph = chartQueries.ClassSizeDistribution("Spring", '2021', "SPPH")
+        allarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
+        totalarr = allarr.sum(axis=1)
+        #line 63 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
+        finalarr = np.concatenate((allarr,totalarr),axis=1)
+    
+    # Note here "result" is the variable by which the HTML will recognize "finalarr" 
+    return render(request, 'filename.html', {"result": finalarr})
+
+
