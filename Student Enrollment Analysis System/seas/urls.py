6d73a38faf245e6f616aae004a6edd01f71e132e
topurayhan007 <topurayhan007@gmail.com>
2021-12-15 11:54:29 +0600

    Made Templates to generate Charts and Table

--- Student Enrollment Analysis System/seas/urls.py
+++ Student Enrollment Analysis System/seas/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('login', views.loginpage, name='loginpage'),
     path('dashboard', views.dashboardpage, name='dashboardpage'),
     path('logout', views.logout_request, name='logout'),
+    path('ClassSizeRequirement', views.ClassSizeRequirementView, name='ClassSizeRequirement'),
+    path('ClassSizeDistribution', views.ClassSizeDistributionView, name='ClassSizeDistribution'),
+    path('UsageOfTheResources', views.UsageOfTheResourcesView, name='UsageOfTheResources'),
+    
 
 ]
--- Student Enrollment Analysis System/seas/views.py
+++ Student Enrollment Analysis System/seas/views.py
@@ -37,50 +37,61 @@ def logout_request(request):
 
 
 def ClassSizeRequirementView(request):
-    if request.method == "POST":
-        # get.year and get.semester from HTML DropDown Selection put that instead of Spring and 2021
-        # Columns: Class Size(label not values from query), Sections, Classroom 6, Classroom 7
-        # Rows: 1-10, 11-20, 21-30, 31-35, 36-40, 41-50, 51-55, 56-65
-        finalarr = chartQueries.ClassSizeRequirement("Spring", '2021')
-        finalarr = np.array(finalarr)
-        # Row last: Total (This row found using code below)
-        totalarr = finalarr.sum(axis=0)
-        #line 49 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
-        table = np.concatenate((finalarr, totalarr), axis=0)
+    # if request.method == "POST":
+    # get.year and get.semester from HTML DropDown Selection put that instead of Spring and 2021
+    # Columns: Class Size(label not values from query), Sections, Classroom 6, Classroom 7
+    # Rows: 1-10, 11-20, 21-30, 31-35, 36-40, 41-50, 51-55, 56-65
+    finalarr = chartQueries.ClassSizeRequirement("Spring", '2021')
+    finalarr = np.array(finalarr)
+    # Row last: Total (This row found using code below)
+    totalarr = finalarr.sum(axis=0)
+    #line 49 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
+    # table = np.concatenate((finalarr, totalarr), axis=0)
 
     # Note here "result" is the variable by which the HTML will recognize "table"   
-    return render(request, 'classSizeRequirement.html', {"result":table})
+    return render(request, 'classSizeRequirement.html', {
+        'result':finalarr,
+        'total': totalarr,
+    })
 
 
 
 def ClassSizeDistributionView(request):
-    if request.method == "POST":
-        # get.year and get.semester from HTML DropDown Selection and put that instead of Spring and 2021
-        sbe = chartQueries.ClassSizeDistribution("Spring", '2021', "SBE")
-        sels = chartQueries.ClassSizeDistribution("Spring", '2021', "SELS")
-        sets = chartQueries.ClassSizeDistribution("Spring", '2021', "SETS")
-        slass = chartQueries.ClassSizeDistribution("Spring", '2021', "SLASS")
-        spph = chartQueries.ClassSizeDistribution("Spring", '2021', "SPPH")
-        # Columns: Size(label not values from query), SBE, SELS, SETS, SLASS, SPPH, Total
-        # Rows: 1-10, 11-20, 21-30, 31-35, 36-40, 41-50, 51-55, 56-60, 60+
-        allarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
-        # Column: Total (This column found using code below)
-        totalarr = allarr.sum(axis=1)
-        #line 70 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
-        finalarr = np.concatenate((allarr,totalarr),axis=1)
+    # if request.method == "POST":
+    # get.year and get.semester from HTML DropDown Selection and put that instead of Spring and 2021
+    sbe = chartQueries.ClassSizeDistribution("Spring", '2021', "SBE")
+    sels = chartQueries.ClassSizeDistribution("Spring", '2021', "SELS")
+    sets = chartQueries.ClassSizeDistribution("Spring", '2021', "SETS")
+    slass = chartQueries.ClassSizeDistribution("Spring", '2021', "SLASS")
+    spph = chartQueries.ClassSizeDistribution("Spring", '2021', "SPPH")
+    # Columns: Size(label not values from query), SBE, SELS, SETS, SLASS, SPPH, Total
+    # Rows: 1-10, 11-20, 21-30, 31-35, 36-40, 41-50, 51-55, 56-60, 60+
+    allarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
+    # Column: Total (This column found using code below)
+    totalarr = allarr.sum(axis=1)
+    #line 70 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
+    # finalarr = np.concatenate((allarr,totalarr),axis=1)
     
     # Note here "result" is the variable by which the HTML will recognize "finalarr" 
-    return render(request, 'ClassSizeDistribution.html', {"result": finalarr})
+    return render(request, 'ClassSizeDistribution.html', {
+        'result': allarr,
+        'total': totalarr,
+
+        
+    })
 
 
 
 def UsageOfTheResourcesView(request):
-    if request.method == "POST":
-        # get.year and get.semester from HTML DropDown Selection and put that instead of Spring and 2021
-        # columns areas follows: Sum, Avg Enroll, Avg Room, Difference, Unused%
-        # rows are as follows: Selected Semester, SBE, SELS, SETS, SLASS, SPPH
-        table = chartQueries.UsageOfTheResources("Spring", '2021')
-    
+    # if request.method == "POST":
+    # get.year and get.semester from HTML DropDown Selection and put that instead of Spring and 2021
+    # columns areas follows: Sum, Avg Enroll, Avg Room, Difference, Unused%
+    # rows are as follows: Selected Semester, SBE, SELS, SETS, SLASS, SPPH
+    table = chartQueries.UsageOfTheResources("Spring", '2021')
+
     # Note here "result" is the variable by which the HTML will recognize "table" 
-    return render(request, 'usageOfTheResources.html', {"result": table})
+    return render(request, 'usageOfTheResources.html', {
+        'result': table
+    
+    })
 
