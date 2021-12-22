c41a29f8e929a19bf02d7e3554541bc374d76856
topurayhan007 <topurayhan007@gmail.com>
2021-12-22 22:51:41 +0600

    Created ClassDistribution Table

--- Student Enrollment Analysis System/seas/views.py
+++ Student Enrollment Analysis System/seas/views.py
@@ -116,15 +116,11 @@ def ClassSizeDistributionView(request):
         # Columns: Size(label not values from query), SBE, SELS, SETS, SLASS, SPPH, Total
         # Rows: Enrollment, 1-10, 11-20, 21-30, 31-35, 36-40, 41-50, 51-55, 56-60, 60+
         rowlabel = ["1-10", "11-20", "21-30", "31-35", "36-40", "41-50", "51-55", "56-60", "60+"]
-        # finalarr has row wise data
-        finalarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
-        # Column: Total (This column found using code below)
-        # totalarr is a column-wise data
-        totalarr = finalarr.sum(axis=1)
-        # schoolList = ["Size", School_T.objects.order_by().values_list('schoolTitle').distinct(), "Total"]
         collabel = ["Enrollment", "SBE", "SELS", "SETS", "SLASS", "SPPH", "Total"]
-        #line 70 not working= ValueError:all the input arrays must have same number of dimensions, but the array at index 0 has 2 dimension(s) and the array at index 1 has 1 dimension(s)
-        # finalarr = np.concatenate((finalarr,totalarr),axis=1)
+        
+        finalarr = np.concatenate((sbe, sels, sets, slass, spph), axis=1)
+        totalarr = finalarr.sum(axis=1)
+        
         table = [collabel]
         count = 0
 
