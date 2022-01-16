34ec3bb29f3cf9a90cb84b2d1cae9a35d01f6258
topurayhan007 <topurayhan007@gmail.com>
2022-01-16 10:39:24 +0600

    Final Version 1.0

--- Student Enrollment Analysis System/Home/settings.py
+++ Student Enrollment Analysis System/Home/settings.py
@@ -15,7 +15,8 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
